798d1434cbfe621eaed0a7716892a00aaa547761
Andrew Munro <Andrew.Munro@yardi.com>
2023-08-21 22:39:03 -0600

    Contact page validation + Add github link

---
+++ js/contact.js
@@ -0,0 +1,35 @@
+﻿function showContactModal() {
+    let myModal = new bootstrap.Modal(document.getElementById('exampleModal'), {
+        keyboard: false
+    });
+    myModal.show()
+}
+document.addEventListener("DOMContentLoaded", function () {
+    if (window.location.href.includes("#thanks")) {
+        let myModal = new bootstrap.Modal(document.getElementById('thanksModal'), {
+            keyboard: false
+        });
+        myModal.show()
+    }
+});
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
